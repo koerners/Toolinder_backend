9f3f9a0e8060a2497ec108a4989925b5714a8295
Stefan Körner <contact@skoerner.com>
2020-04-18 21:18:18 +0200

    Different orientation !important

--- app/responses.py
+++ app/responses.py
@@ -5,7 +5,7 @@ import regex as re
 # Lädt die Fragen aus dem JSON File und gibt sie zurück
 def getQuestions():
     df = pd.read_json(r'./data/questions.json')
-    return df.to_json(orient='index')
+    return df.to_json(orient='records')
 
 
 # Findet das Keyword basierend auf seiner ID
@@ -13,7 +13,7 @@ def getKeywordById(id):
     df = pd.read_json(r'./data/keywords.json')
     # TODO: Hier möchten wir natürlich nur das eine, gefragte Keyword zurückbekommen und nicht alle
     df = df.loc[df['id'].isin([id])]
-    return df.to_json(orient='index')
+    return df.to_json(orient='records')
 
 
 # TODO: Hauptarbeit: Hier kommen die beantworteten Fragen rein und basierend darauf müssen die richtigen tools zurückgegeben werden
@@ -68,6 +68,6 @@ def calculateResult(content):
     print(tools)
 
 
-    return tools.to_json(orient='index')
+    return tools.to_json(orient='records')
 
 # calculateResult(test_input)
